88467daba112a651881291d24a2095516f1c9a3d
julcalde <julcalde@student.42heilbronn.de>
2025-08-09 21:16:13 +0200

    added comment to clarify the differences

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 15:47:10 by julcalde          #+#    #+#             */
-/*   Updated: 2025/08/09 16:03:06 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/08/09 21:12:37 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 int main(void)
 {
+	// Create a Zombie on the heap and announce it
 	Zombie* zombie1 = newZombie("Zombo");
 	zombie1->announce();
 	delete zombie1;
+	// Create a Zombie on the stack and announce it
 	randomChump("kiZombie");
 	return (0);
 }
